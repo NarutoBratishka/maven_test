92e860c084e9d1ee1756ff340dc9aa5924942f8d
NarutoBratishka <katorabian@gmail.com>
2020-11-04 17:35:57 +0700

    add user class for example

--- src/test/resources/test.sql
+++ src/test/resources/test.sql
@@ -1,4 +1,6 @@
 #INSERT INTO book (title, student_id) VALUES ('Metallopocalipse', 4);
 #ALTER TABLE animal RENAME COLUMN anim_desc TO last_name;
 #DESC animal;
-SELECT * FROM animal;
+#CREATE TABLE testdb.users(id INT PRIMARY KEY, userName VARCHAR(40), password VARCHAR(40));
+#INSERT INTO users VALUES (1, 'Greta', 'test'), (2, 'Steve', 'test');
+SELECT * FROM users;
