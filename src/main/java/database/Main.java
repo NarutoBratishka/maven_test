9caf2dddb74167e34bc2dd8729006dfda3789d9c
NarutoBratishka <katorabian@gmail.com>
2020-11-04 17:37:39 +0700

    configure work logic with JDBC

--- src/main/java/database/Main.java
+++ src/main/java/database/Main.java
@@ -9,7 +9,15 @@ public class Main {
         String query = "SELECT * FROM users;";
         try {
             Statement statement = dbCon.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
 
+            while(resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUserName(resultSet.getString("userName"));
+                user.setPassword(resultSet.getString("password"));
+                System.out.println(user);
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
